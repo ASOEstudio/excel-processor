141af27260237944cb90636f1b5aa0a0129b2cb9
ASOEstudio <albert.sdeoliveira@gmail.com>
2020-11-28 15:26:07 -0300

    arruma problema com possível falta de scroll para as tabelas

--- src/app/pages/result-process/result-process.component.ts
+++ src/app/pages/result-process/result-process.component.ts
@@ -3,7 +3,6 @@ import { Router } from '@angular/router';
 
 import { AuxiliaryService, SheetMatch } from 'src/app/services/auxiliary.service';
 
-import { faSort, faSortNumericUpAlt, faSortNumericDown } from '@fortawesome/free-solid-svg-icons';
 import { MatTable } from '@angular/material/table';
 import { DOCUMENT } from '@angular/common';
 
@@ -21,14 +20,6 @@ export class ResultProcessComponent implements OnInit, OnDestroy, AfterViewInit 
 
   public columnsLabel: string[];
   public tabIndex = 0;
-
-  // public sortCredito: null | 'big' | 'small' = 'small';
-
-  // icons
-  // sortIcon = faSort;
-  // sortBigIcon = faSortNumericUpAlt;
-  // sortSmallIcon = faSortNumericDown;
-
   public liberadosAll = false;
 
   constructor(
@@ -63,61 +54,23 @@ export class ResultProcessComponent implements OnInit, OnDestroy, AfterViewInit 
     });
     this.resultBackup = JSON.parse(JSON.stringify(result));
     this.result = result;
-    // this.sortLiberados();
   }
 
   defineTabBodyHeight(): void {
-    setTimeout(() => {
-      const pageBack = this.document.body.querySelector('app-page-back').clientHeight;
-      const card = this.document.body.querySelector('mat-card').clientHeight;
-      const tabHeader = this.document.body.querySelector('mat-tab-header').clientHeight;
-      const tabBody = this.document.body.querySelector('.mat-tab-body-wrapper');
-      const margin = (2 + 2) * 16;
-      const sum = `height: calc(100vh - ${pageBack + card + tabHeader + margin}px)`.toLowerCase();
-      tabBody.setAttribute('style', sum);
-    }, 300);
+    let tabBody: HTMLDivElement = this.document.body.querySelector('.mat-tab-body-wrapper');
+    setInterval(() => {
+      if (!tabBody.style.height) {
+        const pageBack = this.document.body.querySelector('app-page-back').clientHeight;
+        const card = this.document.body.querySelector('mat-card').clientHeight;
+        const tabHeader = this.document.body.querySelector('mat-tab-header').clientHeight;
+        tabBody = this.document.body.querySelector('.mat-tab-body-wrapper');
+        const margin = (2 + 2) * 16;
+        const sum = `height: calc(100vh - ${pageBack + card + tabHeader + margin}px)`.toLowerCase();
+        tabBody.setAttribute('style', sum);
+        console.log(tabBody.style.height);
+      }
+    }, 500);
   }
-
-  // customScroll(): void {
-  //   const tabBodyContent = this.document.body.querySelector('.mat-tab-body-content');
-  //   tabBodyContent.classList.add('ep-custom-scroll');
-  // }
-
-  // sortLiberados(index?: number): void {
-  //   if (typeof index === 'number') { this.result.find( item => item.cpf === 'notas liberadas').dataNf.splice(index, 1); }
-  //   this.result.find( item => item.cpf === 'notas liberadas').dataNf
-  //     .forEach( (line, i) => { if (line.credito === 0) { this.sortLiberados(i); } else { return; } });
-  // }
-
-  // sortCreditoButton(): void {
-  //   if (this.sortCredito === null) { this.sortCredito = 'big';
-  //   } else if (this.sortCredito === 'big') { this.sortCredito = 'small';
-  //   } else if (this.sortCredito === 'small') { this.sortCredito = null; }
-  //   this.sortResult();
-  // }
-
-  // sortResult(): void {
-  //   if (this.sortCredito === null) {
-  //     this.result = this.resultBackup;
-  //   } else if (this.sortCredito === 'big') {
-  //     this.result.forEach( cad => {
-  //       cad.dataNf.sort( (a, b) => {
-  //         if (a.credito < b.credito) { return 1; }
-  //         if (a.credito > b.credito) { return -1; }
-  //         return 0;
-  //       });
-  //     });
-  //   } else if (this.sortCredito === 'small') {
-  //     this.result.forEach( cad => {
-  //       cad.dataNf.sort( (a, b) => {
-  //         if (a.credito > b.credito) { return 1; }
-  //         if (a.credito < b.credito) { return -1; }
-  //         return 0;
-  //       });
-  //     });
-  //   }
-  //   console.log('new result', this.result);
-  // }
 
 }
 
